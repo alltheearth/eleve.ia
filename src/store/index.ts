c2cb35026359d3c3e80d8bba1c8d9a99bff30d42
Lucas Pires <lcpires7.dev@gmail.com>
2025-11-05 10:39:49 -0300

    upload projeto base

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,13 @@
 // src/store/index.ts - CORRIGIDO
 import { configureStore } from "@reduxjs/toolkit";
-import ModuleActiveSlice from "../Feature/ModuleActiveSlice";
-import AuthSlice from "../Feature/AuthSlice";
-import { schoolApi } from "../services/schoolApi";
-import { faqsApi } from "../services/faqsApi";
-import { eventsApi } from "../services/eventsApi";
-import { contactsApi } from "../services/contactsApi";
-import { leadsApi } from "../services/leadsApi";
-import { uzapiApi } from "../services/uzapiApi";
+
 
 const store = configureStore({
     reducer: {
-        moduleActive: ModuleActiveSlice,
-        auth: AuthSlice,
-        [schoolApi.reducerPath]: schoolApi.reducer,
-        [faqsApi.reducerPath]: faqsApi.reducer,
-        [eventsApi.reducerPath]: eventsApi.reducer,
-        [contactsApi.reducerPath]: contactsApi.reducer,
-        [leadsApi.reducerPath]: leadsApi.reducer,
-        [uzapiApi.reducerPath]: uzapiApi.reducer,
+      
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
-            schoolApi.middleware, 
-            faqsApi.middleware, 
-            eventsApi.middleware,
-            contactsApi.middleware,
-            leadsApi.middleware,
-            uzapiApi.middleware
         ),
 })
 
